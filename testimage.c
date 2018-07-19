c42379e8186be118d80b6cccf1c1b2388ce6984b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2018-07-19 16:59:16 +0200

    updated test image to have numbers

--- testimage.c
+++ testimage.c
@@ -1,5 +1,100 @@
 #include <stdio.h>
 
+#define GLYPH_WIDTH 5
+#define GLYPH_HEIGHT 8
+
+const char glyph_0[] = {
+0, 0, 0, 0, 0,
+0, 1, 1, 1, 0,
+1, 0, 0, 0, 1,
+1, 0, 0, 1, 1,
+1, 0, 1, 0, 1,
+1, 1, 0, 0, 1,
+1, 0, 0, 0, 1,
+0, 1, 1, 1, 0,
+};
+
+const char glyph_1[] = {
+0, 0, 0, 0, 0,
+0, 0, 1, 0, 0,
+0, 1, 1, 0, 0,
+0, 0, 1, 0, 0,
+0, 0, 1, 0, 0,
+0, 0, 1, 0, 0,
+0, 0, 1, 0, 0,
+0, 1, 1, 1, 0,
+};
+
+const char glyph_2[] = {
+0, 0, 0, 0, 0,
+0, 1, 1, 1, 0,
+1, 0, 0, 0, 1,
+0, 0, 0, 0, 1,
+0, 0, 0, 1, 0,
+0, 0, 1, 0, 0,
+0, 1, 0, 0, 0,
+1, 1, 1, 1, 1,
+};
+
+const char glyph_3[] = {
+0, 0, 0, 0, 0,
+1, 1, 1, 1, 1,
+0, 0, 0, 1, 0,
+0, 0, 1, 0, 0,
+0, 0, 0, 1, 0,
+0, 0, 0, 0, 1,
+1, 0, 0, 0, 1,
+0, 1, 1, 1, 0,
+};
+
+const char glyph_4[] = {
+0, 0, 0, 0, 0,
+0, 0, 0, 1, 0,
+0, 0, 1, 1, 0,
+0, 1, 0, 1, 0,
+1, 0, 0, 1, 0,
+1, 1, 1, 1, 1,
+0, 0, 0, 1, 0,
+0, 0, 0, 1, 0,
+};
+
+const char glyph_5[] = {
+0, 0, 0, 0, 0,
+1, 1, 1, 1, 1,
+1, 0, 0, 0, 0,
+1, 1, 1, 1, 0,
+0, 0, 0, 0, 1,
+0, 0, 0, 0, 1,
+1, 0, 0, 0, 1,
+0, 1, 1, 1, 0,
+};
+
+const char glyph_6[] = {
+0, 0, 0, 0, 0,
+0, 0, 1, 1, 0,
+0, 1, 0, 0, 0,
+1, 0, 0, 0, 0,
+1, 1, 1, 1, 0,
+1, 0, 0, 0, 1,
+1, 0, 0, 0, 1,
+0, 1, 1, 1, 0,
+};
+
+const char glyph_7[] = {
+0, 0, 0, 0, 0,
+1, 1, 1, 1, 1,
+1, 0, 0, 0, 1,
+0, 0, 0, 1, 0,
+0, 0, 1, 0, 0,
+0, 1, 0, 0, 0,
+0, 1, 0, 0, 0,
+0, 1, 0, 0, 0,
+};
+
+const char *glyphs[] = {
+glyph_0, glyph_1, glyph_2, glyph_3, glyph_4, glyph_5, glyph_6, glyph_7
+};
+
 void square(FILE *fp, int xpos, int ypos, int w, int h, int c)
 {
     int x, y;
@@ -12,10 +107,27 @@ void square(FILE *fp, int xpos, int ypos, int w, int h, int c)
     }
 }
 
+void glyph(FILE *fp, const char *g, int xpos, int ypos, int c)
+{
+    int x, y;
+    int pos;
+
+    pos = 0;
+    for(y = 0; y < GLYPH_HEIGHT; y++) {
+        for(x = 0; x < GLYPH_WIDTH; x++) {
+            if(g[pos]) {
+                fprintf(fp, "%d %d %d pxpt\n", xpos + x, ypos + y, c);
+            }
+            pos++;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     FILE *fp;
     int x;
+    int xoff, yoff;
 
     fp = fopen("test.rnt", "w");
 
@@ -25,6 +137,11 @@ int main(int argc, char *argv[])
         square(fp, x * 32, 0, 32, 32 * (x + 1), x % 8);
     }
 
+    xoff = 16 - (GLYPH_WIDTH / 2);
+    yoff = 4;
+    for(x = 0; x < 8; x++) {
+        glyph(fp, glyphs[x], xoff + x * 32, yoff, 7 - x);
+    }
     fprintf(fp, "\"out.png\" pxsave\n");
     fclose(fp);
     return 0;
